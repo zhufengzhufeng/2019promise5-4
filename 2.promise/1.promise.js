b1306e61d207945c9cd3d49e4d4c7ee641fda715
fandonglai <704072802@qq.com>
2019-05-04 15:49:17 +0800

    promise-resolvePromise

--- 2.promise/1.promise.js
+++ 2.promise/1.promise.js
@@ -9,24 +9,23 @@ function read(url){
         })
     })
 }
-// 1) 如果then方法返回的是一个常量 包括undefined，会把这个结果传递给外层的then的成功的结果 
-// 2) 如果then方法中抛出异常 会走到下一次then的失败的结果
-// 3) 穿透 如果没有处理错误 会继续向下找,会就近查找错误处理函数 如果没找到会一直找，一般情况会写一个catch方法,catch是不会中断运行的
-// 4) then方法执行后可能会返回一个promise,那么会采用这个promise的返回结果作为下一个then的成功或者失败
-// 5) 走失败 两种可能 第一种发生了错误 第二种就是返回一个失败的promise
-// 6) finally 方法 es9 的 不会终端执行，只是传递一个一定会执行的函数而已
+// 1) 循环引用 保证返回的promise不是当前then返回的promise 否则就变成了自己等待自己完成
 let p1 = new Promise((resolve,reject)=>{
     resolve(100);
 })
-let promise2 = p1.then((data)=>{
-    return data;
-})
-promise2.then(data=>{
+let promise2 = p1.then(()=>{
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(new Promise((resolve,reject)=>{
+               setTimeout(()=>{
+                    resolve(10000)
+               },1000)
+            }));
+        },1000)
+    })
+});
+promise2.then((data)=>{
     console.log(data);
 },err=>{
     console.log('err',err);
 })
-    
-
-// then jq链式调用 promise一旦成功就不能失败了
-// promise 需要每次调用then后都返回一个新的promise 可以实现状态的切换
--- 2.promise/history/3/promise.js
+++ 2.promise/history/3/promise.js
@@ -1,40 +1,6 @@
-let  resolvePromise = (promise2,x,resolve,reject) => {
+function resolvePromise(promise2,x,resolve,reject){
     // 判断x的类型 来处理promise2是成功还是失败
-    // 所有的promise都遵循这个规范，不同的人写的promise可能会混用
-    // 尽可能考虑的周全 要考虑别人promise可能出错的情况
-    if(promise2 === x){
-        return reject(new TypeError('循环引用'))
-    }
-    // 判断x是不是一个promise  ,这个x 可能不是自己的promise 所以为了安全 需要在进行校验，放置调一起用成功和失败 
-    if(typeof x === 'function' || (typeof x === 'object' && x !== null)){
-        // 尝试取当前x的then方法, 这个then方法可能别人定义的时候 用的Object.defineProperty 
-        let called;
-        try{
-            let then = x.then; // 如果取then方法出错 那就用错误拒绝promise2
-            if(typeof then === 'function'){ // 我就认为他是一个promise
-                then.call(x,y=>{ // 让当前的promise执行 ，不用多次取then方法了
-                    // y 有可能还是一个promise , 继续调用resolvePromise方法，直到解析出一个常量为止，最终把常量传递下去
-                    if(called) return; // 放置此方法多次被调用
-                    called = true;
-                    resolvePromise(promise2,y,resolve,reject);
-                },r=>{
-                    if(called) return;
-                    called = true;
-                    reject(r); // 让当前的promise变成失败态即可
-                })
-            }else{
-                // x就是一个普通的对象 并没有then方法
-                resolve(x);
-            }
-        }catch(e){
-            if(called) return;
-            called = true;
-            reject(e);
-        }
-    }else{
-        // x肯定一个常量
-        resolve(x);
-    }
+    console.log(promise2);
 }
 
 class Promise{
@@ -66,13 +32,14 @@ class Promise{
     }
     then(onfulfilled,onrejected){ 
         let promise2;
+        //调用then后必须返回一个新的promise
         promise2 = new Promise((resolve,reject)=>{
             if(this.status === 'fulfilled'){
-                setTimeout(()=>{ 
+                setTimeout(()=>{ // 为了保证promise2存在
                     try{
+                        // 需要对then的成功的回调和失败的回调取到他的返回结果，如果是普通值就让promise2成功即可
                         let x = onfulfilled(this.value);
-                        // x是普通值还是promise 如果是普通值 直接调用promise2的resolve
-                        // 如果是promise  那应该让x这个promise执行 x.then
+                        // 对x的类型做判断，常量可以直接抛出来 但是如果是promise需要采取当前promise的状态
                         resolvePromise(promise2,x,resolve,reject);
                     }catch(e){
                         reject(e);
@@ -90,6 +57,7 @@ class Promise{
                 },0)
             }
             if(this.status === 'pending'){
+                // ... 先把成功的回调和失败的回调分开存放
                 this.resolveCallbacks.push(()=>{
                     setTimeout(()=>{
                         try{
@@ -113,7 +81,6 @@ class Promise{
             }
         });
         return promise2
-        
     }
 }
 module.exports = Promise;
